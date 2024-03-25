8f23a0060a290077f0cf1d6aefa88c45aa88e40f
Abdullah <abdullahblrr@gmail.com>
2024-03-25 18:45:26 +0300

    usref 2 örneği yapıldı

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
-  return <>Merhaba</>;
+  const [sayı, setSayı] = useState(0);
+  const [can, setCan] = useState(3);
+  const sayıRef = useRef(0);
+
+  useEffect(() => {
+    sayıRef.current++;
+    console.log("Sayı Değişti");
+    console.log("Sayıref Değişti ", sayıRef.current - 1);
+  }, [sayı]);
+
+  return (
+    <div>
+      <p>
+        Sayı: {sayı} Can:{can}
+      </p>
+      <button
+        onClick={() => {
+          setSayı((eskideğer) => eskideğer + 5);
+        }}
+      >
+        Sayı Artır (+5)
+      </button>
+
+      <button
+        onClick={() => {
+          setSayı((eskideğer) => eskideğer - 3);
+        }}
+      >
+        Sayı Artır (-3)
+      </button>
+      <button
+        onClick={() => {
+          setCan((eskideğer) => eskideğer - 1);
+        }}
+      >
+        Sayı Artır (-3)
+      </button>
+    </div>
+  );
 }
 
 export default App;
