3d9bbbb11a597effcca9624f55e3a18e0c040620
Abdullah <abdullahblrr@gmail.com>
2024-03-25 18:26:47 +0300

    useref örneği yapıldı

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useRef, useState } from "react";
 
 function App() {
-  return <>Merhaba</>;
+  const [sayac, setSayac] = useState(0);
+  const puan = useRef(0);
+
+  function setPuan() {
+    puan.current += 10;
+  }
+
+  return (
+    <>
+      <div>
+        App {puan.current}
+        <div>
+          {sayac}
+          <button
+            onClick={() => {
+              setSayac((eskideğer) => eskideğer + 5);
+            }}
+          >
+            Sayac Güncelle
+          </button>
+        </div>
+        <div>
+          <button onClick={setPuan}>Ref Değerini Güncelle</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
