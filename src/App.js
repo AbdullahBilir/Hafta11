0afb7ffa17e039cee5237a6a61eaed848baf58bb
Abdullah <abdullahblrr@gmail.com>
2024-03-26 14:41:56 +0300

    use-ref 3 örneği yapıldı

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return <>Merhaba</>;
+import { useEffect } from "react";
+import { useRef } from "react";
+
+function App({ prop }) {
+  const useRefPrev = useRef(prop);
+
+  useEffect(() => {
+    console.log("Önceki Değer", useRefPrev.current);
+    console.log("Güncel Değer", prop);
+    useRefPrev.current = prop;
+  }, [prop]);
+
+  return (
+    <div>
+      <p>Prop: {prop}</p>
+    </div>
+  );
 }
 
 export default App;
