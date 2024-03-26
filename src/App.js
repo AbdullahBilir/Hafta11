fef066055fe7080d86617f720f1ee130a27e12bb
Abdullah <abdullahblrr@gmail.com>
2024-03-26 17:35:48 +0300

    useref timing örneği yapıldı

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
-function App() {
-  return <>App</>;
+import { useEffect, useRef, useState } from "react";
+
+function Timer() {
+  const [sayac, setSayac] = useState(0);
+  const intervalRef = useRef();
+  const startTimer = () => {
+    intervalRef.current = setInterval(() => {
+      console.log("timetick");
+    }, 1000);
+  };
+
+  const stopTimer = () => {
+    clearInterval(intervalRef.current);
+  };
+
+  return (
+    <div>
+      {intervalRef.current}
+      <button onClick={startTimer}>Start Timer</button>
+      <button onClick={stopTimer}>Stop Timer</button>
+      <br />
+      {sayac}
+      <button
+        onClick={() => {
+          setSayac((eskideğer) => eskideğer + 5);
+        }}
+      >
+        Arttır
+      </button>
+    </div>
+  );
 }
 
-export default App;
+export default Timer;
