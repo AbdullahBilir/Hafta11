3c2ae21ea1efcaa8f1803fc2d145430e0f8ec31a
Abdullah <abdullahblrr@gmail.com>
2024-03-26 14:44:24 +0300

    GÜNCELLENDİ

--- src/App.js
+++ src/App.js
@@ -1,20 +1,5 @@
-import { useEffect } from "react";
-import { useRef } from "react";
-
-function App({ prop }) {
-  const useRefPrev = useRef(prop);
-
-  useEffect(() => {
-    console.log("Önceki Değer", useRefPrev.current);
-    console.log("Güncel Değer", prop);
-    useRefPrev.current = prop;
-  }, [prop]);
-
-  return (
-    <div>
-      <p>Prop: {prop}</p>
-    </div>
-  );
+function App() {
+  return <>App</>;
 }
 
 export default App;
